40645e6ad3a1fa7f06edcfe3b807b87f581c1fc2
IvanLarionov90 <gnosticism100500@gmail.com>
2023-02-20 16:03:11 +0500

    functionality with modal windows

--- dist/script.js
+++ dist/script.js
@@ -10,8 +10,9 @@
 
 __webpack_require__.r(__webpack_exports__);
 const modals = () => {
+  let btnPressed;
   function bindModal(triggerSelector, modalSelector, closeSelector) {
-    let closeClickOverlay = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
+    let destroy = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
@@ -22,8 +23,13 @@ const modals = () => {
         if (e.target) {
           e.preventDefault();
         }
+        btnPressed = true;
+        if (destroy) {
+          item.remove();
+        }
         windows.forEach(item => {
           item.style.display = 'none';
+          item.classList.add('animated', 'fadeIn');
         });
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
@@ -39,7 +45,7 @@ const modals = () => {
       document.body.style.marginRight = `0px`;
     });
     modal.addEventListener('click', e => {
-      if (e.target === modal && closeClickOverlay) {
+      if (e.target === modal) {
         windows.forEach(item => {
           item.style.display = 'none';
         });
@@ -60,6 +66,8 @@ const modals = () => {
       if (!display) {
         document.querySelector(selector).style.display = 'block';
         document.body.style.overflow = "hidden";
+        let scroll = calcScroll();
+        document.body.style.marginRight = `${scroll}px`;
       }
     }, time);
   }
@@ -74,10 +82,21 @@ const modals = () => {
     div.remove();
     return scrollWidth;
   }
+  function openByScroll(selector) {
+    window.addEventListener('scroll', () => {
+      if (!btnPressed && window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        document.querySelector(selector).click();
+      }
+    });
+  }
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-  showModalByTime('.popup-consultation', 60000);
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+  openByScroll('.fixed-gift');
+
+  // showModalByTime('.popup-consultation', 3000)
 };
+
 /* harmony default export */ __webpack_exports__["default"] = (modals);
 
 /***/ })
