248d8d56ba0ff0bac12620ba78d0fb65af4112a4
IvanLarionov90 <gnosticism100500@gmail.com>
2023-03-09 15:30:16 +0500

    add accordion and burger

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,39 @@
+const accordion = (triggersSelector) => {
+    
+    // const buttons = document.querySelectorAll(triggersSelector),
+    //       blocks = document.querySelectorAll(itemsSelector);
+        
+    // blocks.forEach(block => {
+    //     block.classList.add('animated', 'fadeInDown');
+    // });
+
+    // buttons.forEach(btn => {
+    //     btn.addEventListener('click', function() {
+    //         if (!this.classList.contains('active')) {
+    //             buttons.forEach(btn => {
+    //                 btn.classList.remove('active', 'active-style');
+    //             });
+    //             this.classList.add('active', 'active-style');
+    //         }
+    //     })
+    // })
+
+    const buttons = document.querySelectorAll(triggersSelector);
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', function() {
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+
+            if (this.classList.contains('active-style')) {
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px';
+            } else {
+                this.nextElementSibling.style.maxHeight = '0px'
+            }
+        });
+    });
+
+
+};
+
+export default accordion;
